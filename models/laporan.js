9ec8371de46fe28b10e03459ed56d286060a5f46
najwazzah <najwaazzahrap@gmail.com>
2025-06-11 16:10:05 +0700

    Update laporan.js

--- models/laporan.js
+++ models/laporan.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-
 const laporanSchema = new mongoose.Schema({
     nama: String,
-    isi: String,
+    judul: String,
+    detail: String,
+    instansi: String,
+    jenis: String,
+    lampiran: String,
     status: { type: String, default: 'Belum diproses' },
     tanggal: { type: Date, default: Date.now }
 });
-
-module.exports = mongoose.model('Laporan', laporanSchema);
